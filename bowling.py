4964a4ccbf029b26b71f3c14ec9f44b9c50635cc
Joe K <joe@storage0.flag.rootnode.com>
2012-02-07 21:34:15 -0600

    added padding to python version

--- bowling.py
+++ bowling.py
@@ -1,5 +1,6 @@
 
 
+
 def score_frame(remaining_throws):
 	if (remaining_throws[0] == 10 and len(remaining_throws) >= 3):
                 score = 10 + remaining_throws[1] + remaining_throws[2]
@@ -23,7 +24,9 @@ def score_game(frame_number, throws):
 
 
 def bowl(throwlist):
+        padded_throwlist = throwlist + ([0] * 21)
         return score_game(0, throwlist)
+
 
 
 
