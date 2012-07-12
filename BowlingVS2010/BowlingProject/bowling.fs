7add538c1a82c125f5ec6e9a9bbdc43ad802cec8
Joe Koberg <joe.koberg@versionone.com>
2012-07-12 12:26:19 -0400

    Added VS project

---
+++ BowlingVS2010/BowlingProject/bowling.fs
@@ -0,0 +1,43 @@
+﻿module bowling
+
+type Roll = Roll of int
+
+type FrameType =
+  | Strike
+  | Spare
+  | Normal
+
+type ScoreResult =
+  | Frame of FrameType * int * list<Roll>
+  | RollAgain of list<Roll>
+
+type FrameResult = FrameResult of int * ScoreResult
+
+type GameResult =
+  | FinishedGame of int * list<FrameResult>
+  | UnfinishedGame of string * list<FrameResult>
+  
+
+let score nextrolls =
+  match nextrolls with
+  | Roll r1 :: (Roll b1 ::  Roll b2 :: _ as more)  when r1 = 10       -> Frame(Strike, 10 + b1 + b2, more)
+  | Roll r1 ::  Roll r2 :: (Roll b1 :: _ as more)  when r1 + r2 = 10  -> Frame(Spare, r1 + r2 + b1, more)
+  | Roll r1 ::  Roll r2 :: (_ as more)                                -> Frame(Normal, r1 + r2, more)
+  | ( _ as more)                                                      -> RollAgain(more)
+  
+
+let rec gameloop totalscore framenumber resultlist rollsleft =
+  let framenumber = framenumber + 1
+  if framenumber > 10 
+  then FinishedGame(totalscore, List.rev resultlist)
+  else match score rollsleft with
+       | RollAgain(unscored_rolls) -> UnfinishedGame("Ran out of rolls", List.rev resultlist)
+       | Frame(_, framescore, remainder) as result ->
+         let newscore = totalscore + framescore
+         let newresults = FrameResult(framenumber, result) :: resultlist
+         gameloop newscore framenumber newresults remainder
+
+
+let bowling_game = gameloop 0 0 []
+
+
---
+++ BowlingVS2010/BowlingProject/bowling_tests.fs
@@ -0,0 +1,44 @@
+﻿module bowling_tests
+
+open bowling
+
+type TestResult =
+  | Success
+  | Failure
+
+let bowling_test  expected_score  rolls =
+  let result = bowling_game rolls
+  printfn "%A" result
+  match result with
+  | FinishedGame(expected_score, _) -> Success
+  | _ -> Failure
+
+
+let repeat items = seq { while true do yield! items }
+
+let makeListOf count items = repeat items |> Seq.take count |> List.ofSeq
+
+open NUnit.Framework
+
+
+[<TestFixture>]
+type BowlingTests() =
+
+    member self.test(expected_score, roll_list) =
+      let result = bowling_test expected_score roll_list
+      assert (result = Success)
+
+    [<Test>]
+    member self.test_dutch() = self.test(200, makeListOf 17 [Roll 5; Roll 5; Roll 10])
+
+    [<Test>]
+    member self.test_perfect() = self.test(300, makeListOf 12 [Roll 10])
+    
+    [<Test>]
+    member self.test_normal() = self.test(60, makeListOf 20 [Roll 3])
+    
+    [<Test>]
+    member self.test_short() = self.test(100, makeListOf 5 [Roll 3])
+    
+    [<Test>]
+    member self.test_gutter() = self.test(0, makeListOf 20 [Roll 0])
