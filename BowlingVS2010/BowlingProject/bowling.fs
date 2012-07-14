6710951f3e5729f799af11e436fd089e1301eced
jkoberg <joe.koberg@versionone.com>
2012-07-13 22:09:45 -0300

    changes

--- BowlingVS2010/BowlingProject/bowling.fs
+++ BowlingVS2010/BowlingProject/bowling.fs
@@ -1,14 +1,14 @@
 module bowling
 
-type ThrowResult = Drop of int
+type Throw = Drop of int
 
 type FrameType = Strike | Spare | Normal
 
-type RollResult =
-  | ClosedFrame of FrameType * int * list<Roll>
-  | OpenFrame of list<Roll>
-
 type Frame = Frame of int * FrameType * int
+
+type ThrowResult =
+  | ClosedFrame of FrameType * int * list<Throw>
+  | OpenFrame of list<Throw>
 
 type GameResult =
   | FinishedGame of int * list<Frame>
@@ -17,23 +17,25 @@ type GameResult =
 
 let score throws =
   match throws with
-  | Drop 10 :: (Drop b1 ::  Drop b2 :: _ as upcoming)                   -> ClosedFrame(Strike, 10+b1+b2, upcoming)
-  | Drop r1 ::  Drop r2 :: (Drop b1 :: _ as upcoming)  when r1+r2 = 10  -> ClosedFrame(Spare,  r1+r2+b1, upcoming)
-  | Drop r1 ::  Drop r2 :: (_ as upcoming)                              -> ClosedFrame(Normal, r1+r2,    upcoming)
-  | (_ as upcoming)                                                     -> OpenFrame(upcoming)
-  
-let rec gameloop total framecount results throws =
-  if framecount >= 10 
-  then FinishedGame(total, List.rev results)
-  else match (score throws) with
+  | Drop 10 :: (Drop b1 ::  Drop b2 :: _ as upcoming)                  -> ClosedFrame(Strike, 10+b1+b2, upcoming)
+  | Drop r1 ::  Drop r2 :: (Drop b1 :: _ as upcoming) when r1+r2 = 10  -> ClosedFrame(Spare,  r1+r2+b1, upcoming)
+  | Drop r1 ::  Drop r2 :: (_ as upcoming)                             -> ClosedFrame(Normal, r1+r2,    upcoming)
+  | (_ as upcoming)                                                    -> OpenFrame(upcoming)
+
+
+let rec gameloop framecount results throws =
+  if framecount > 10 then
+    let total = Seq.sum <| seq { for Frame(_,_,score) in results -> score }
+    FinishedGame(total, List.rev results)
+  else
+    match (score throws) with
        | OpenFrame(_)
          -> UnfinishedGame("Ran out of throws", List.rev results)
+       | ClosedFrame(typ, score, upcoming)
+         let result = Frame(framecount, typ, score)
+         -> gameloop (framecount+1) (result::results) upcoming
 
-       | ClosedFrame(typ, score, upcoming) as result
-         -> let result = Frame(framecount, typ, score)
-            gameloop (total+score) (framecount+1) (result::results) upcoming
 
-
-let bowling_game = gameloop 0 0 []
+let score_bowling_game = gameloop 1 []
 
 
