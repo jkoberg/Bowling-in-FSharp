7984434ef47bc206f6eca5a1ec599d10fdef31da
jkoberg <joe.koberg@versionone.com>
2012-07-13 21:11:47 -0300

    cleanup

--- BowlingVS2010/BowlingProject/bowling_tests.fs
+++ BowlingVS2010/BowlingProject/bowling_tests.fs
@@ -1,44 +1,45 @@
-﻿module bowling_tests
+module bowling_tests
 
 open bowling
-
-type TestResult =
-  | Success
-  | Failure
-
-let bowling_test  expected_score  rolls =
-  let result = bowling_game rolls
-  printfn "%A" result
-  match result with
-  | FinishedGame(expected_score, _) -> Success
-  | _ -> Failure
-
-
-let repeat items = seq { while true do yield! items }
-
-let makeListOf count items = repeat items |> Seq.take count |> List.ofSeq
-
 open NUnit.Framework
-
 
 [<TestFixture>]
 type BowlingTests() =
 
-    member self.test(expected_score, roll_list) =
-      let result = bowling_test expected_score roll_list
-      assert (result = Success)
+    let repeat items =
+      seq { while true do yield! items }
+
+    let makeListOf count items =
+      repeat items |> Seq.take count |> List.ofSeq
+
+    type TestResult = Success | Failure
+
+    member self.do_test(expected_score, throws) =
+      let result = bowling_game throws
+      printfn "%A" result
+      let outcome =
+        match result with
+          | FinishedGame(expected_score, _) -> Success
+          | _ -> Failure
+      assert (outcome = Success)
 
     [<Test>]
-    member self.test_dutch() = self.test(200, makeListOf 17 [Roll 5; Roll 5; Roll 10])
+    member self.test_dutch() =
+      self.do_test(200, makeListOf 17 [Drop 5; Drop 5; Drop 10])
 
     [<Test>]
-    member self.test_perfect() = self.test(300, makeListOf 12 [Roll 10])
+    member self.test_perfect() =
+      self.do_test(300, makeListOf 12 [Drop 10])
     
     [<Test>]
-    member self.test_normal() = self.test(60, makeListOf 20 [Roll 3])
+    member self.test_normal() =
+      self.do_test(60, makeListOf 20 [Drop 3])
     
     [<Test>]
-    member self.test_short() = self.test(100, makeListOf 5 [Roll 3])
+    member self.test_short() =
+      self.do_test(100, makeListOf 5 [Drop 3])
     
     [<Test>]
-    member self.test_gutter() = self.test(0, makeListOf 20 [Roll 0])
+    member self.test_gutter() =
+      self.test(0, makeListOf 20 [Drop 0])
+
