7fd3f487abed1d9c0c79efaa5849b2f181e1db37
jkoberg <joe.koberg@versionone.com>
2012-07-13 21:01:57 -0300

    cleanup

--- BowlingVS2010/BowlingProject/bowling.fs
+++ BowlingVS2010/BowlingProject/bowling.fs
@@ -15,23 +15,23 @@ type GameResult =
   | UnfinishedGame of string * list<Frame>
   
 
-let score rolls =
-  match rolls with
+let score throws =
+  match throws with
   | Drop 10 :: (Drop b1 ::  Drop b2 :: _ as upcoming)                   -> ClosedFrame(Strike, 10+b1+b2, upcoming)
   | Drop r1 ::  Drop r2 :: (Drop b1 :: _ as upcoming)  when r1+r2 = 10  -> ClosedFrame(Spare,  r1+r2+b1, upcoming)
   | Drop r1 ::  Drop r2 :: (_ as upcoming)                              -> ClosedFrame(Normal, r1+r2,    upcoming)
   | (_ as upcoming)                                                     -> OpenFrame(upcoming)
   
-let rec gameloop total framecount results nextrolls =
+let rec gameloop total framecount results throws =
   if framecount >= 10 
   then FinishedGame(total, List.rev results)
-  else match (score nextrolls) with
+  else match (score throws) with
        | OpenFrame(_)
-         -> UnfinishedGame("Ran out of rolls", List.rev results)
+         -> UnfinishedGame("Ran out of throws", List.rev results)
 
-       | ClosedFrame(type, score, nextrolls) as result
-         -> let result = Frame(framecount, type, score)
-            gameloop (total+score) (framecount+1) (result::results) nextrolls
+       | ClosedFrame(typ, score, upcoming) as result
+         -> let result = Frame(framecount, typ, score)
+            gameloop (total+score) (framecount+1) (result::results) upcoming
 
 
 let bowling_game = gameloop 0 0 []
